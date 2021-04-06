4e0fbf4ef67855a82e43b820875e59c2307d6639
MuhammadRadynalAffandi <79552025+MuhammadRadynalAffandi@users.noreply.github.com>
2021-04-06 18:27:59 +0700

    Update NPC.java

--- src/NPC.java
+++ src/NPC.java
@@ -1,18 +1,15 @@
 import java.util.ArrayList;
 
 public class NPC extends Interactable {
-    //kunci dipindahkan dari ruangan
     private Item objKunci;
     private boolean isKenal = false;
 
     public NPC() {
         nama = "NPC";
 
-        //init kunci
         objKunci = new Item("Kunci");
         objKunci.setDeskripsi("Sebuah kunci kecil yang sudah agak berkarat");
 
-        //aksi npc
         arrAksi.add("Perkenalan dgn NPC");
         arrAksi.add("Minta kunci");
 
@@ -20,20 +17,17 @@ public class NPC extends Interactable {
 
     @Override
     public void prosesAksi(int pil) {
-        //1: perkenalan dengan npc
-        //2: buka pintu
         if (pil == 1) {
             System.out.println("Halo saya penjaga pintu ini");
             isKenal = true;
         } else if (pil == 2) {
             if (isKenal) {
-                //berikan kunci pada player
                 if (objKunci==null) {
                     System.out.println("Masa lupa, kunci kan sudah saya berikan!");
                 } else
                 {
                     System.out.println("Kunci diberikan pada player");
-                    objGameInfo.getObjPlayer().addItem(objKunci);     //tambahkan  objek ini pada player
+                    objGameInfo.getObjPlayer().addItem(objKunci);
                     objKunci = null;
                 }
 
