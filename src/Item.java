cbbbf5cec8eb4173c3ef04d1b47191711d8b1aa3
MuhammadRadynalAffandi <79552025+MuhammadRadynalAffandi@users.noreply.github.com>
2021-04-06 18:27:09 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -2,25 +2,20 @@ import java.util.ArrayList;
 
 public class Item extends Interactable{
     private String deskripsi;
-    private Ruangan objRuangan;  //ruangan tempat item, jika null artinya item dipegang npc atau plyaer
-
-    //constructor
+    private Ruangan objRuangan;
+    
     public Item(String nama) {
         this.nama = nama;
     }
 
     @Override
     public void prosesAksi(int pil) {
-        //pilihan user untuk aksi yang akan diambil
-        //urutan harus sama dengan isi arrAksi
         if (pil==1) {
             System.out.println(deskripsi);
-        } else  if (pil==2) {  //bisa ambil atau buang
+        } else  if (pil==2) { 
             if (objRuangan==null) {
-               //dipegang player, buang ke ruangan
                 dibuang();
             } else {
-                //ada di ruangan, diambil player
                 diambil();
             }
         }
@@ -28,36 +23,31 @@ public class Item extends Interactable{
 
     private void dibuang() {
         System.out.println("Item dibuang player ke ruangan");
-        objGameInfo.getObjPlayer().hapusItem(this); //hapus dari player
-        objGameInfo.getObjRuangan().addItem(this);  //tambah ke ruangan
-        objRuangan = objGameInfo.getObjRuangan(); // set ruangan
+        objGameInfo.getObjPlayer().hapusItem(this);
+        objGameInfo.getObjRuangan().addItem(this);
+        objRuangan = objGameInfo.getObjRuangan();
     }
 
 
-    //pindahkan item dari ruangan ke player
     private void diambil() {
         System.out.println("Item diambil player");
-        objGameInfo.getObjPlayer().addItem(this);     //tambahkan  objek ini (this) pada player
-        objRuangan.hapusItem(this);                    //hapus dari ruangan
+        objGameInfo.getObjPlayer().addItem(this);
+        objRuangan.hapusItem(this);
         objRuangan = null;
     }
 
 
     public void printItem() {
-        //print deskripsi item
         System.out.println(deskripsi);
     }
 
     @Override
     public ArrayList<String> getAksi() {
-        //aksi dinamik tergantung ada di ruangan atau dipegang player/npc
         ArrayList<String> arrOut = new ArrayList<>();
         if (objRuangan==null) {
-            //ada di player, ada opsi buang
             arrOut.add("Deskripsi Item");
             arrOut.add("Buang item");
         } else {
-            //ada di ruangan ada opsi ambil
             arrOut.add("Deskripsi Item");
             arrOut.add("Ambil item");
         }
